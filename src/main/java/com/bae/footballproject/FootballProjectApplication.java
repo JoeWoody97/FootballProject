f387cac6b26a7855c85c2eed811945431ddddb2d
Joe Woodward <joewoodward2097@gmail.com>
2021-03-01 18:44:07 +0000

    setting project up in eclipse

---
+++ src/main/java/com/bae/footballproject/FootballProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bae.footballproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballProjectApplication.class, args);
+	}
+
+}
