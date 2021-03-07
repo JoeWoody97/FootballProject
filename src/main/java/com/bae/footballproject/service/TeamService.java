fd456e9d1a74def8440fd20606f2b599ce4957c9
Joe Woodward <joewoodward2097@gmail.com>
2021-03-07 14:19:40 +0000

    Setup the controller, service and the repo

---
+++ src/main/java/com/bae/footballproject/service/TeamService.java
@@ -0,0 +1,19 @@
+package com.bae.footballproject.service;
+
+import java.util.List;
+
+import com.bae.footballproject.domain.Team;
+
+public interface TeamService {
+
+	Team createTeam(Team team);
+
+	Team getTeamById(Long id);
+	
+	Team updateTeamById(Long id, Team newTeam);
+
+	boolean deleteTeamById(Long id);
+
+	List<Team> getTeam();
+
+}
